d79cd40b0728405e1258209d6fa9557255ae76b2
Junhao Zhang <junhaodev@gmail.com>
2020-08-26 21:09:25 -0500

    Add Welcome Script

---
+++ welcome.js
@@ -0,0 +1,3 @@
+console.log(`\x1b[1m\x1b[33mWelcome to cookeylang!\x1b[0m
+  \x1b[94mVisit \x1b[4mhttps://cookeylangteam.repl.co \x1b[0m\x1b[94mfor documentation.\x1b[0m
+  \x1b[92mSpecial thanks to @Coder100, @JDOG787, @CodingCactus, @Codemonkey51, @RaidTheWeb, and @TheSummit3145!\x1b[0m`)
